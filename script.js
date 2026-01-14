06f127b3dfd81adce19588b3282e7437fedad020
Vedant Dadhich <vedantdadhich21@gmail.com>
2026-01-14 23:48:30 +0530

    added grids

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("working")
+
+const container = document.querySelector(".container");
+
+const blocks = document.createElement("div");
+blocks.classList.add("blocks");
+
+const grids = document.querySelector("#grid")
+
+createBlocks();
+
+grids.addEventListener("input" ,createBlocks )
+
+function createBlocks(){
+    console.log("i ran")
+    let noOfGrid = grids.value;
+let height = 720/noOfGrid ;
+let width = height;
+
+blocks.style.height = height + "px";
+blocks.style.width  = width  + "px";
+blocks.style.boxSizing = "border-box";
+blocks.style.backgroundColor = "white";
+blocks.style.border = "1px solid rgb(232, 232, 232)";
+
+    const boxes = document.querySelectorAll(".blocks");
+    boxes.forEach(box => box.remove());
+
+    for(let i =0;i< noOfGrid*noOfGrid;i++){
+    container.appendChild(blocks.cloneNode(true));
+    }
+
+}
+
+
